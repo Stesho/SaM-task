f484240388178c652901abb91f0cb710a1dcaa78
Steshenko Dmitriy <3301297@mail.ru>
2022-11-06 17:34:37 +0300

    feat: add header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Header.module.scss';
+import logo from '../../assets/img/logo.jpg';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <nav className={styles.nav}>
+          <div className={styles.logo}>
+            <span>Book</span>
+            <img src={logo} alt="logo" />
+            <span>Shop</span>
+          </div>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
