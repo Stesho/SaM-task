7ea6e08787a1a8ae8b851e753f88bfd3dd921ad0
Steshenko Dmitriy <3301297@mail.ru>
2022-11-09 02:08:13 +0300

    feat: add theme toggle

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import useTheme from "../../hooks/useTheme";
 import styles from "./Header.module.scss";
-import { ReactComponent as Logo } from "../../assets/img/logo.svg";
+import { ReactComponent as Logo } from "../../assets/svg/logo.svg";
 
 function Header() {
+  const [theme, toggleTheme] = useTheme();
+
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
+      <div className={[styles.headerContainer, styles.container].join(" ")}>
         <nav className={styles.nav}>
           <div className={styles.logo}>
             <span>Book</span>
@@ -13,6 +16,18 @@ function Header() {
             <span>Shop</span>
           </div>
         </nav>
+        <div className={styles.themeSwitcher}>
+          <div className={styles.themeSwitcher__caption}>Theme</div>
+          <label className={styles.themeSwitcher__label} htmlFor="themeToggle">
+            <input
+              id="themeToggle"
+              type="checkbox"
+              onChange={(event) => toggleTheme(event.target.checked)}
+              checked={theme === "dark"}
+            />
+            <div className={styles.themeSwitcher__btn} />
+          </label>
+        </div>
       </div>
     </header>
   );
---
+++ src/hooks/useTheme.js
@@ -0,0 +1,21 @@
+import { useLayoutEffect, useState } from "react";
+
+const useTheme = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("color-theme") || "light"
+  );
+
+  const toggleTheme = (isChecked) => {
+    const newTheme = isChecked ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("color-theme", newTheme);
+  };
+
+  useLayoutEffect(() => {
+    document.documentElement.setAttribute("color-theme", theme);
+  }, [theme]);
+
+  return [theme, toggleTheme];
+};
+
+export default useTheme;
