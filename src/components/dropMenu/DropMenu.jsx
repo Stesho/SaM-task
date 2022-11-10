af941a222e06ff257edfbde6cde6498a054f0256
Steshenko Dmitriy <3301297@mail.ru>
2022-11-10 09:04:24 +0300

    feat: implement drop menu closing after click outside

--- src/components/dropMenu/DropMenu.jsx
+++ src/components/dropMenu/DropMenu.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './DropMenu.module.scss';
 
 function DropMenu({ caption, className, children }) {
   const [isOpen, setIsOpen] = useState(false);
+  const menu = useRef();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   }
 
+  useEffect(() => {
+    const onClick = (event) => menu.current.contains(event.target) || setIsOpen(false)
+    document.addEventListener('click', onClick);
+    return () => document.removeEventListener('click', onClick);
+  }, []);
+
   return (
-    <div className={styles.dropMenu}>
+    <div ref={menu} className={styles.dropMenu}>
       <button type='button' className={className} onClick={toggleMenu}>
         {caption}
       </button>
